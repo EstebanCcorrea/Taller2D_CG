c2761e364c264fb01dd337032d69a5a519ab0f6e
jesusalba15 <jesus.albarracin@uao.edu.co>
2025-10-03 18:36:42 -0500

    .

--- Taller2D_CG/Assets/Scripts/ControllerScenes/GameControllerScene2.cs
+++ Taller2D_CG/Assets/Scripts/ControllerScenes/GameControllerScene2.cs
@@ -1,37 +1,16 @@
 using UnityEngine;
-using TMPro;
 
-public class ControllerScene : MonoBehaviour
+public class GameControllerScene2 : MonoBehaviour
 {
-    [Header("UI Resumen")]
-    public GameObject panelResumen;   // Panel de resumen oculto en la escena
-    public TMP_Text itemsTotalText;   // Texto para mostrar total de ítems
-
-    private void Start()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        if (panelResumen != null)
-            panelResumen.SetActive(false);
+        
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Update is called once per frame
+    void Update()
     {
-        if (collision.CompareTag("Player"))
-        {
-            MostrarResumen(collision.gameObject);
-        }
-    }
-
-    void MostrarResumen(GameObject player)
-    {
-        if (panelResumen != null)
-            panelResumen.SetActive(true);
-
-        // Mostrar total de ítems guardados en el GameManager
-        int totalItems = GameManager.Instance.GetTotalItems();
-        itemsTotalText.text = "Ítems obtenidos: " + totalItems;
-
-        // Congelar movimiento del player
-        Movimiento mov = player.GetComponent<Movimiento>();
-        if (mov != null) mov.enabled = false;
+        
     }
 }
